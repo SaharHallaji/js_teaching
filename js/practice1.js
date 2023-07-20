38704246f88d4d6f735348cda1536403768154e9
sahar <saharhallaji.dev@gmail.com>
2023-07-20 16:32:35 +0330

    first problem set

---
+++ js/practice1.js
@@ -0,0 +1,8 @@
+const fruits = ["apple" , "banana" , "orange" , "kiwi" , "cherry" , "mango"]
+let counter = 0 ;
+if (fruits.length > 0){
+    for (let i = 0; i < fruits.length; i++) {
+        if (fruits[i].indexOf("a") !== -1 ) counter ++;
+    }
+    console.log(counter)
+}else console.log("The Array Is Empty!")
