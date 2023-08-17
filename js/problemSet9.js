8b6cee8f5a682e190de09147177fb7925d88af7e
sahar <saharhallaji.dev@gmail.com>
2023-08-17 23:43:17 +0330

    problem set 9

--- js/problemSet9.js
+++ js/problemSet9.js
@@ -1,56 +1,41 @@
-class Rectangle{
-    height = 0
-    width = 0
-    constructor(height , width) {
-        this.height = height
-        this.width = width
-    }
-    area(){
-        return (this.width * this.height)/2
-    }
-    environment(){
-        return this.width * 3
-    }
-}
-
-const area = new Rectangle(10 , 15).area()
-console.log(area)
-//////////////////////////////////////////////////////////////////////////////
-
 class Person {
     fName = null
     lName = null
     age = null
-    degree = null
     phoneNumber = null
+    degree = null
     id = null
-    constructor(firstName,lastName) {
-        this.id = this.generateId()
+
+    constructor(firstName, lastName) {
         this.fName = firstName
         this.lName = lastName
+        this.id = this.generateId()
     }
-    generateId (){
+
+    generateId() {
         const dateString = Date.now().toString(36);
-        const randomness = Math.random().toString(36).substring(2);
+        const randomness = Math.random().toString(36).substring(2)
         return dateString + randomness;
     }
-    set setAge(age){
+    set setAge(age) {
         this.age = age
     }
-    set setDegree(deg){
+
+    set SetNumber(number) {
+        this.phoneNumber = number
+    }
+
+    set setDegree(deg) {
         this.degree = deg
     }
-    set setPhoneNumber(phoneNumber){
-        this.phoneNumber = phoneNumber
-    }
-    get info(){
-        return `name  : ${this.fName} ${this.lName} \nage: ${this.age} \ndegree : ${this.degree} \nphone number : ${this.phoneNumber}`
-    }
 
+    get info() {
+        return `name: ${this.fName} ${this.lName} \nage: ${this.age} \ndegree: ${this.degree} \nphone number: ${this.phoneNumber}`
+    }
 }
 
-const person = new Person("Sahar" , "Hallaji")
-person.setAge = 21
+const person = new Person("sahar" , "hallaji")
 person.setDegree = "bachelor"
-person.setPhoneNumber = "0913"
+person.setAge = 21
+person.SetNumber = "0913"
 console.log(person.info)
