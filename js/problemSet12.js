1ececf011b4bd97ff97092e387295ce69bd66aa4
sahar <saharhallaji.dev@gmail.com>
2023-08-21 23:39:53 +0330

    problem set 9

--- js/problemSet12.js
+++ js/problemSet12.js
@@ -4,21 +4,25 @@ const phoneNumber = document.querySelector("input[name ='name']")
 const result = document.querySelector("p")
 
 const add = () => {
-    if (phoneNumber.value.length === 13 && name.value.length > 5) {
+    if (name.value.length > 3) {
         if (phoneNumber.value.indexOf("+98") === 0) {
             numbers.push({name: name.value, phoneNumber: phoneNumber.value})
-        } else alert("enter a number witch begins with +98")
-    } else alert("enter a name and a number please")
+        } else if (phoneNumber.value.indexOf("0") === 0) {
+            numbers.push({name: name.value, phoneNumber: phoneNumber.value.replace("0", "+98 ")})
+        } else if (phoneNumber.value.indexOf("9") === 0) {
+            numbers.push({name: name.value, phoneNumber: "+98 " + phoneNumber.value})
+        }else alert("Please enter a valid number!")
+    } else alert("Enter a name and a number please")
     show()
 }
 const show = () => {
     result.innerHTML = ""
     for (let i = 0; i < numbers.length; i++) {
-        result.innerHTML += `${i + 1} - ${numbers[i]["name"]} : ${numbers[i]["phoneNumber"]} <span onclick="remove(${i})"> x </span>`
+        result.innerHTML += `${i + 1} - ${numbers[i]["name"]} : ${numbers[i]["phoneNumber"]} <button onclick="remove(${i})"> x </button> <br>`
     }
 }
 
-const remove = (index)=>{
-    numbers.splice(index , 1)
+const remove = (index) => {
+    numbers.splice(index, 1)
     show()
 }
